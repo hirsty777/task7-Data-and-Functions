edc00fda54cee78f1e02012276c2b3401a1f066a
hirsty777 <777hirsty@gmail.com>
2022-12-07 16:57:10 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3>{{ separateRef }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button>Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+ 
+<script>
+import {ref} from 'vue';
+
+export default {
+  setup(){
+    const separateRef = ref('Learn vue.js(Separate ref)');
+    const refObject = ref({myCorseGoal:'Learn vue.js(Separate ref)'});
+
+    return { separateRef }
+  }
+}
+</script>
+
+
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
